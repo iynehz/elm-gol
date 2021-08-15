a7f6276f42dd869941c787be2bf5ba20dbf5c56e
Stephan Loyd <stephanloyd9@gmail.com>
2021-08-15 20:47:13 +0800

    tidy up, and reformat ..

--- src/Main.elm
+++ src/Main.elm
@@ -1,51 +1,59 @@
 module Main exposing (..)
 
+import Bootstrap.Button
+import Bootstrap.CDN
 import Browser
+import Browser.Navigation exposing (back)
+import GameOfLife.Grid as Grid
+import GameOfLife.Pattern as Pattern
+import Html exposing (Html)
+import Html.Attributes
+import List
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events
-import Html exposing (Html)
-import Html.Events
-import Html.Attributes
 import Time
-import List
-import Json.Decode
-import Debug
-import Array
-import Array2D
 
-import Bootstrap.CDN
-import Bootstrap.Button
 
-import GameOfLife.Grid as Grid
-import GameOfLife.Pattern as Pattern
-import Browser.Navigation exposing (back)
 
 -- Constants
 
+
 gridSize : Grid.Size
-gridSize = Grid.Size 80 60 
+gridSize =
+    Grid.Size 80 60
+
 
 cellSize : Int
-cellSize = 10
+cellSize =
+    10
+
 
 gridColor : String
-gridColor = "grey"
+gridColor =
+    "grey"
+
 
 backgroundColor : String
-backgroundColor = "white"
+backgroundColor =
+    "white"
+
 
 cellColor : String
-cellColor = "black"
+cellColor =
+    "black"
 
--- tick period in mS
+
 tickPeriod : Float
-tickPeriod = 100
+tickPeriod =
+    100
 
-type alias Model = 
-    { grid: Grid.Grid
-    , state: State
+
+type alias Model =
+    { grid : Grid.Grid
+    , state : State
     }
+
 
 type Msg
     = Tick Time.Posix
@@ -55,14 +63,18 @@ type Msg
     | Clear
     | GridClicked Grid.Position
 
+
 type State
     = Active
     | Inactive
 
-init : () -> (Model, Cmd Msg)
-init _ = initGame ()
 
-initGame : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
+init _ =
+    initGame ()
+
+
+initGame : () -> ( Model, Cmd Msg )
 initGame _ =
     ( { grid = Pattern.addPattern Pattern.gliderGun (Grid.initialize gridSize)
       , state = Inactive
@@ -70,54 +82,83 @@ initGame _ =
     , Cmd.none
     )
 
-update : Msg -> Model -> (Model, Cmd Msg) 
-update msg model = 
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
     case msg of
-        Tick time -> 
+        Tick time ->
             let
-                grid = model.grid
+                grid =
+                    model.grid
+
                 newModel =
                     { model
-                    | grid = if model.state == Active then Grid.evolve grid else grid }
+                        | grid =
+                            if model.state == Active then
+                                Grid.evolve grid
+
+                            else
+                                grid
+                    }
             in
-                ( newModel
-                , Cmd.none
-                )
-        Start -> 
-            let
-                newModel =
-                    { model
-                    | state = Active }
-            in
-                (newModel, Cmd.none)
-        Stop -> 
-            let
-                newModel =
-                    { model
-                    | state = Inactive }
-            in
-                (newModel, Cmd.none)
-        Reset -> initGame ()
-        Clear -> 
+            ( newModel
+            , Cmd.none
+            )
+
+        Start ->
             let
                 newModel =
                     { model
-                    | grid = Grid.clear model.grid
-                    , state = Inactive }
+                        | state = Active
+                    }
             in
-                (newModel, Cmd.none)
-        GridClicked pos -> 
-            let 
-                cell = Maybe.withDefault False (Array2D.get pos.x pos.y grid)
-                grid = model.grid
+            ( newModel, Cmd.none )
+
+        Stop ->
+            let
                 newModel =
                     { model
-                    | grid = if model.state == Inactive then Grid.set pos (not cell) grid else grid }
+                        | state = Inactive
+                    }
             in
-                (newModel, Cmd.none)
+            ( newModel, Cmd.none )
+
+        Reset ->
+            initGame ()
+
+        Clear ->
+            let
+                newModel =
+                    { model
+                        | grid = Grid.clear model.grid
+                        , state = Inactive
+                    }
+            in
+            ( newModel, Cmd.none )
+
+        GridClicked pos ->
+            let
+                cell =
+                    Maybe.withDefault False (Grid.get pos grid)
+
+                grid =
+                    model.grid
+
+                newModel =
+                    { model
+                        | grid =
+                            if model.state == Inactive then
+                                Grid.set pos (not cell) grid
+
+                            else
+                                grid
+                    }
+            in
+            ( newModel, Cmd.none )
+
 
 view : Model -> Html Msg
-view model = 
+view model =
     Html.div []
         [ Bootstrap.CDN.stylesheet
         , Html.h1 [] [ text "Conway's Game of Life" ]
@@ -126,86 +167,98 @@ view model =
             [ Bootstrap.Button.button
                 [ Bootstrap.Button.onClick Start
                 , Bootstrap.Button.disabled (model.state == Active)
-                , if model.state == Inactive then Bootstrap.Button.primary else Bootstrap.Button.secondary ]
+                , if model.state == Inactive then
+                    Bootstrap.Button.primary
+
+                  else
+                    Bootstrap.Button.secondary
+                ]
                 [ text "Start" ]
             , Html.text " "
             , Bootstrap.Button.button
                 [ Bootstrap.Button.onClick Stop
                 , Bootstrap.Button.disabled (model.state == Inactive)
-                , if model.state == Active then Bootstrap.Button.primary else Bootstrap.Button.secondary ]
+                , if model.state == Active then
+                    Bootstrap.Button.primary
+
+                  else
+                    Bootstrap.Button.secondary
+                ]
                 [ text "Stop" ]
             , Html.text " "
             , Bootstrap.Button.button
                 [ Bootstrap.Button.onClick Clear
-                , Bootstrap.Button.secondary ]
+                , Bootstrap.Button.secondary
+                ]
                 [ text "Clear" ]
             , Html.text " "
             , Bootstrap.Button.button
                 [ Bootstrap.Button.onClick Reset
-                , Bootstrap.Button.secondary ]
+                , Bootstrap.Button.secondary
+                ]
                 [ text "Reset" ]
             ]
         ]
 
+
 subscriptions : Model -> Sub Msg
-subscriptions model = Time.every tickPeriod Tick
+subscriptions model =
+    Time.every tickPeriod Tick
+
 
 renderGrid : Model -> Html Msg
-renderGrid model = 
+renderGrid model =
     let
-        canvasWidth = gridSize.width * cellSize
-        canvasHeight = gridSize.height * cellSize
-        cellsList = Grid.toIndexedList model.grid
+        canvasWidth =
+            gridSize.width * cellSize
+
+        canvasHeight =
+            gridSize.height * cellSize
+
+        cellsList =
+            Grid.toIndexedList model.grid
     in
-        svg
+    svg
+        [ width (String.fromInt canvasWidth)
+        , height (String.fromInt canvasHeight)
+        , viewBox (String.join " " <| List.map String.fromInt [ 0, 0, canvasWidth, canvasHeight ])
+        ]
+        ([ rect
             [ width (String.fromInt canvasWidth)
             , height (String.fromInt canvasHeight)
-            , viewBox (String.join " " <| List.map String.fromInt [0, 0, canvasWidth, canvasHeight])
+            , stroke gridColor
+            , fill backgroundColor
             ]
-            (
-                [
-                    rect
-                    [ width (String.fromInt canvasWidth)
-                    , height (String.fromInt canvasHeight)
-                    , stroke gridColor
-                    , fill backgroundColor
-                    ] []
-                ]
-                ++ (List.map (\x -> renderCell (Tuple.first x) (Tuple.second x)) cellsList)
-                ++ (List.map (
-                    \x -> line
-                    [ x1 (String.fromInt (x * cellSize))
-                    , x2 (String.fromInt (x * cellSize))
-                    , y1 "0"
-                    , y2 (String.fromInt canvasHeight)
-                    , stroke gridColor
-                    ] [])
-                    (List.range 0 gridSize.width))
-                ++ (List.map (
-                    \x -> line
-                    [ y1 (String.fromInt (x * cellSize))
-                    , y2 (String.fromInt (x * cellSize))
-                    , x1 "0"
-                    , x2 (String.fromInt canvasWidth)
-                    , stroke gridColor
-                    ] [])
-                    (List.range 0 gridSize.height))
-            )
+            []
+         ]
+            ++ List.map (\x -> renderCell (Tuple.first x) (Tuple.second x)) cellsList
+        )
+
 
 renderCell : Grid.Position -> Bool -> Svg Msg
 renderCell pos isLive =
-  rect [ width (String.fromInt cellSize)
-       , height (String.fromInt cellSize)
-       , x (String.fromInt (pos.x * cellSize))
-       , y (String.fromInt (pos.y * cellSize))
-       , if isLive then fill cellColor else fill backgroundColor
-       , Svg.Events.onClick (GridClicked (Grid.Position pos.x pos.y))
-       ]
-       []
+    rect
+        [ width (String.fromInt cellSize)
+        , height (String.fromInt cellSize)
+        , x (String.fromInt (pos.x * cellSize))
+        , y (String.fromInt (pos.y * cellSize))
+        , stroke gridColor
+        , fill
+            (if isLive then
+                cellColor
 
-main = Browser.element
-    { init = init
-    , update = update
-    , view = view
-    , subscriptions = subscriptions
-    }
+             else
+                backgroundColor
+            )
+        , Svg.Events.onClick (GridClicked (Grid.Position pos.x pos.y))
+        ]
+        []
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
